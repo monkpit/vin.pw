169bd5543ca2fc1a102386136bb0491629afc088
Kyle Pittman <pittman.kyle@gmail.com>
2015-09-24 20:42:13 -0500

    Working!

--- vin_flask.py
+++ vin_flask.py
@@ -1,12 +1,13 @@
 #! /usr/bin/env python
 
 from flask import Flask, render_template
+from secrets import API_KEY
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return render_template('vinpw/main.html')
+	return render_template('vinpw/main0.html', API_KEY=API_KEY)
 
 
 if __name__ == "__main__":
