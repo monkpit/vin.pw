20e02976255f3c2085f20f39866ff0d5a69cb6b4
Kyle Pittman <pittman.kyle@gmail.com>
2015-09-24 21:33:37 -0500

    Logs searched vins to db

--- settings.py
+++
@@ -1,5 +0,0 @@
-import os
-from secrets import MYSQL_USER, MYSQL_PASS
-APP_ROOT = os.path.dirname(os.path.abspath(__file__))
-APP_STATIC = os.path.join(APP_ROOT, 'static')
-DATABASE_URI = "mysql+pymysql://{user}:{password}@localhost/vinpw".format(user=MYSQL_USER, password=MYSQL_PASS)
